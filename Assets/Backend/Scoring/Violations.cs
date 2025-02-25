af327734758a6c49b1198b1036bd61c22afa006f
Gradient Descent <48599511+SGD2718@users.noreply.github.com>
2025-02-25 11:51:59 -0800

    Started Adding Vitals

--- Assets/Backend/Scoring/Violations.cs
+++ Assets/Backend/Scoring/Violations.cs
@@ -1,7 +1,39 @@
+using UnityEngine.InputSystem.LowLevel;
+
 namespace Backend.Scoring
 {
     public enum Violations
     {
+        NoPpe = ViolationSeverityLevels.Moderate,
+        FaultyPpe = ViolationSeverityLevels.Minor,
         
+        IgnoredHazard = ViolationSeverityLevels.Minor,
+        AteOrDrankInLab = ViolationSeverityLevels.Moderate,
+        LeftTrippingHazard = ViolationSeverityLevels.Moderate, 
+        
+        CreatedFumes = ViolationSeverityLevels.Moderate,
+        CreatedLethalFumes = ViolationSeverityLevels.Major,
+        
+        FailedToDisposeWaste = ViolationSeverityLevels.Moderate,
+        MixingChemicals = ViolationSeverityLevels.Moderate,
+        RecycledUsedChemicals = ViolationSeverityLevels.Fired,
+        
+        CausedSmallExplosion = ViolationSeverityLevels.Major,
+        CausedBigExplosion = ViolationSeverityLevels.Fired,
+        
+        StartedSmallFire = ViolationSeverityLevels.Major,
+        StartedBigFire = ViolationSeverityLevels.Fired,
+        IneffectiveFireExtinguishing = ViolationSeverityLevels.Minor,
+        HazardousFireExtinguishing = ViolationSeverityLevels.Major,
+        
+        MinorInjury = ViolationSeverityLevels.Moderate,
+        MinorDamage = ViolationSeverityLevels.Moderate,
+        
+        MajorInjury = ViolationSeverityLevels.Major,
+        MajorDamage = ViolationSeverityLevels.Fired,
+        
+        Murder = ViolationSeverityLevels.Fired,
+        Death = ViolationSeverityLevels.InstantLose,
+        Concussion = ViolationSeverityLevels.InstantLose
     }
 }
