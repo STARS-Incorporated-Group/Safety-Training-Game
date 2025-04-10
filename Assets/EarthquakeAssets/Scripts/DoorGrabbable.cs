a7c07c3cbe9c5570312ca391cde3cf40fc85f7fe
YC1964 <ychung5325@gmail.com>
2025-04-10 10:47:32 -0700

    vr door (etest scene)

---
+++ Assets/EarthquakeAssets/Scripts/DoorGrabbable.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DoorGrabbable : XRGrabInteractable
+{
+    public Transform handler;
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+
+        transform.position = handler.transform.position;
+        transform.rotation = handler.transform.rotation;
+
+        Rigidbody rbhandler = handler.GetComponent<Rigidbody>();
+        rbhandler.velocity = Vector3.zero;
+        rbhandler.angularVelocity = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(handler.position, transform.position) > 0.4f)
+        {
+            GetComponent<XRBaseInteractor>().enableInteractions = false;
+        }
+    }
+}
