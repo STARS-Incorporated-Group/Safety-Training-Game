5fb7f58f51440b8d43eefe371159565041d93bf6
Gradient Descent <48599511+SGD2718@users.noreply.github.com>
2025-04-22 10:56:09 -0700

    idk

--- Assets/GameMenuManager.cs
+++ Assets/GameMenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class GameMenuManager : MonoBehaviour
 {
@@ -9,20 +10,21 @@ public class GameMenuManager : MonoBehaviour
     public float spawnDistance = 2;
     public GameObject menu;
     public InputActionProperty showButton;
+
+    public bool active = false;
     
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if(showButton.action.WasPressedThisFrame())
+        if(menu.activeSelf != this.active)
         {
-            menu.SetActive(!menu.activeSelf);
-
+            menu.SetActive(this.active);
             menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
         }
 
