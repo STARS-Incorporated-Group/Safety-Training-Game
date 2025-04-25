419e17c801ee548c03219a483eb90dcd07d21351
YC1964 <ychung5325@gmail.com>
2025-04-25 11:26:58 -0700

    reverted abstract menu

--- Assets/Backend/Menu/AbstractMenu.cs
+++ Assets/Backend/Menu/AbstractMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 namespace Backend.Menu
 {
     [RequireComponent(typeof(Canvas))]
-    public abstract class AbstractMenu : MonoBehaviour
+    public abstract class AbstractMenu : ScriptableObject
     {
         protected MenuManager manager { get; private set; }
         private bool _active;
@@ -14,6 +14,7 @@ namespace Backend.Menu
         [SerializeField] protected float                spawnDistance = 2f;
         [SerializeField] protected GameObject           menu;
         [SerializeField] protected GameObject           parent;
+        [SerializeField] protected InputActionReference showAction;
 
         protected virtual void Initialize(MenuManager menuManager)
         {
@@ -30,6 +31,9 @@ namespace Backend.Menu
             // 2) Hide on start
             _active = false;
             menu.SetActive(false);
+
+            // 3) Hook the pause/show input
+            showAction.action.performed += OnShowAction;
             
             // 4) Give subclasses a chance to hook their buttons
             WireButtons();
@@ -49,9 +53,20 @@ namespace Backend.Menu
         {
         }
 
+
+        protected virtual void OnEnable()  => showAction.action.Enable();
+        protected virtual void OnDisable() => showAction.action.Disable();
+
         protected virtual void OnDestroy()
         {
+            showAction.action.performed -= OnShowAction;
             UnwireButtons();
+        }
+
+        private void OnShowAction(InputAction.CallbackContext ctx)
+        {
+            if (_active) Close();
+            else        Load();
         }
 
         public virtual void Load()
