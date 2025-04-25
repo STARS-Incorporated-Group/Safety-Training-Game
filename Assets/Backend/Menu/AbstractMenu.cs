8a9af4cd7f300d6a103e7d8b97e965f8652d358b
Gradient Descent <48599511+SGD2718@users.noreply.github.com>
2025-04-25 11:32:06 -0700

    Fixed Abstract Menu

--- Assets/Backend/Menu/AbstractMenu.cs
+++ Assets/Backend/Menu/AbstractMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 namespace Backend.Menu
 {
     [RequireComponent(typeof(Canvas))]
-    public abstract class AbstractMenu : MonoBehavior
+    public abstract class AbstractMenu : MonoBehaviour
     {
         protected MenuManager manager { get; private set; }
         private bool _active;
@@ -14,7 +14,6 @@ namespace Backend.Menu
         [SerializeField] protected float                spawnDistance = 2f;
         [SerializeField] protected GameObject           menu;
         [SerializeField] protected GameObject           parent;
-        [SerializeField] protected InputActionReference showAction;
 
         protected virtual void Initialize(MenuManager menuManager)
         {
@@ -31,11 +30,8 @@ namespace Backend.Menu
             // 2) Hide on start
             _active = false;
             menu.SetActive(false);
-
-            // 3) Hook the pause/show input
-            showAction.action.performed += OnShowAction;
             
-            // 4) Give subclasses a chance to hook their buttons
+            // 3) Give subclasses a chance to hook their buttons
             WireButtons();
         }
 
@@ -54,19 +50,12 @@ namespace Backend.Menu
         }
 
 
-        protected virtual void OnEnable()  => showAction.action.Enable();
-        protected virtual void OnDisable() => showAction.action.Disable();
+        protected virtual void OnEnable()  => Load();
+        protected virtual void OnDisable() => Close();
 
         protected virtual void OnDestroy()
         {
-            showAction.action.performed -= OnShowAction;
             UnwireButtons();
-        }
-
-        private void OnShowAction(InputAction.CallbackContext ctx)
-        {
-            if (_active) Close();
-            else        Load();
         }
 
         public virtual void Load()
